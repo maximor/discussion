e5bd03a9cd01e4ee6f0bb6a2c5d28ebe9607d9d2
Maximo Rodriguez <maximo1225@gmail.com>
2020-01-07 21:26:32 -0400

    Creation of role entity

---
+++ src/main/java/com/maximorodriguez/discussion/rest/role/Role.java
@@ -0,0 +1,53 @@
+package com.maximorodriguez.discussion.rest.role;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @CreatedDate
+    @CreationTimestamp
+    private Date creationDate;
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(Date creationDate, String name) {
+        this.creationDate = creationDate;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
